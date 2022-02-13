9c7b06729076774d824ccc325bcd0a38eed3945e
fafa-a <fabienpuech1@gmail.com>
2022-02-13 16:28:47 +0100

    ?

--- form/index.js
+++ form/index.js
@@ -1,30 +1,3 @@
-function isAlpha(val) {
-  var ok = false
-  if (val != "") {
-    var regex = /^[a-zA-Z\-]*$/
-    ok = regex.test(val)
-  }
-  return ok
-}
-
-function isNumerique(val) {
-  var ok = false
-  if (val != "") {
-    var regex = /^[0-9\.]*$/
-    ok = regex.test(val)
-  }
-  return ok
-}
-
-function isEmail(val) {
-  var ok = false
-  if (val != "") {
-    var regex = /^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$/i
-    ok = regex.test(val)
-  }
-  return ok
-}
-
 const form = document.querySelector("form")
 const inputName = document.getElementById("name")
 const inputFirstName = document.getElementById("firstName")
@@ -33,57 +6,57 @@ const inputEmail = document.getElementById("email")
 const divInfo = document.getElementById("info")
 
 const error = function (el) {
-  el.style.backgroundColor = "red"
+    el.style.backgroundColor = "red"
 }
 
 if (form) {
-  form.addEventListener("submit", function (event) {
-    event.preventDefault()
-    const name = inputName.value
-    const firstName = inputFirstName.value
-    const phone = inputPhone.value
-    const email = inputEmail.value
+    form.addEventListener("submit", function (event) {
+        event.preventDefault()
+        const name = inputName.value
+        const firstName = inputFirstName.value
+        const phone = inputPhone.value
+        const email = inputEmail.value
 
-    const allInputValue = {
-      Nom: name,
-      Prénom: firstName,
-      Téléphone: phone,
-      Email: email,
-    }
+        const allInputValue = {
+            Nom: name,
+            Prénom: firstName,
+            Téléphone: phone,
+            Email: email,
+        }
 
-    const isNameValid = isAlpha(name)
-    if (name != "" && !isNameValid) {
-      error(inputName.parentNode)
-    } else {
-      inputName.parentNode.style.backgroundColor = "initial"
-    }
-    const isFirstNameValid = isAlpha(firstName)
-    if (firstName != "" && !isFirstNameValid) {
-      error(inputFirstName.parentNode)
-    } else {
-      inputFirstName.parentNode.style.backgroundColor = "initial"
-    }
-    const isPhoneValid = isNumerique(phone)
-    if (phone != "" && !isPhoneValid) {
-      error(inputPhone.parentNode)
-    } else {
-      inputPhone.parentNode.style.backgroundColor = "initial"
-    }
+        const isNameValid = isAlpha(name)
+        if (name != "" && !isNameValid) {
+            error(inputName.parentNode)
+        } else {
+            inputName.parentNode.style.backgroundColor = "initial"
+        }
+        const isFirstNameValid = isAlpha(firstName)
+        if (firstName != "" && !isFirstNameValid) {
+            error(inputFirstName.parentNode)
+        } else {
+            inputFirstName.parentNode.style.backgroundColor = "initial"
+        }
+        const isPhoneValid = isNumerique(phone)
+        if (phone != "" && !isPhoneValid) {
+            error(inputPhone.parentNode)
+        } else {
+            inputPhone.parentNode.style.backgroundColor = "initial"
+        }
 
-    const isEmailValid = isEmail(email)
-    if (email != "" && !isEmailValid) {
-      error(inputEmail.parentNode)
-    } else {
-      inputEmail.parentNode.style.backgroundColor = "initial"
-    }
+        const isEmailValid = isEmail(email)
+        if (email != "" && !isEmailValid) {
+            error(inputEmail.parentNode)
+        } else {
+            inputEmail.parentNode.style.backgroundColor = "initial"
+        }
 
-    if (isNameValid && isFirstNameValid && isPhoneValid && isEmailValid) {
-      divInfo.classList.add("success")
-      for (const key in allInputValue) {
-        const p = document.createElement("p")
-        p.textContent = `${key} : ${allInputValue[key]}`
-        divInfo.appendChild(p)
-      }
-    }
-  })
+        if (isNameValid && isFirstNameValid && isPhoneValid && isEmailValid) {
+            divInfo.classList.add("success")
+            for (const key in allInputValue) {
+                const p = document.createElement("p")
+                p.textContent = `${key} : ${allInputValue.key}`
+                divInfo.appendChild(p)
+            }
+        }
+    })
 }
