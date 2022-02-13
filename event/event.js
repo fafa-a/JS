84f6653e946617759adda9507f0a7d0c4b9bb319
fafa-a <fabienpuech1@gmail.com>
2022-02-13 23:09:59 +0100

    lastest version

--- event/event.js
+++ event/event.js
@@ -4,29 +4,14 @@ const firstButton = document.querySelector("body  button:first-of-type")
 const secondButton = document.createElement("button")
 const p = document.createElement("p")
 secondButton.innerText = "Bouton 2(v1)"
+secondButton.style.visibility = "hidden"
 firstDiv.appendChild(secondButton)
 
-// first version
-const addEventToButton = function () {
-    firstButton.addEventListener("click", function () {
-        p.innerText = "Mon premier bouton fonctionne"
-    })
-    secondButton.addEventListener("click", function () {
-        p.innerText = "Mon second bouton fonctionne"
-    })
-    secondDiv.appendChild(p)
-}
-addEventToButton()
-// second version
-// const addEventToButton = function () {
-//   const button = arguments[0] === "firstButton" ? firstButton : secondButton
-//   button.addEventListener("click", function () {
-//     p.innerText =
-//       button === firstButton
-//         ? "Mon premier bouton fonctionne"
-//         : "Mon second bouton fonctionne"
-//   })
-//   secondDiv.appendChild(p)
-// }
-// addEventToButton("firstButton")
-// addEventToButton()
+firstButton.addEventListener("click", function () {
+    p.innerText = "Mon premier bouton fonctionne"
+    secondButton.style.visibility = "visible"
+})
+secondButton.addEventListener("click", function () {
+    p.innerText = "Mon second bouton fonctionne"
+})
+secondDiv.appendChild(p)            
