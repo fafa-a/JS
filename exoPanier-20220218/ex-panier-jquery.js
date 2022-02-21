4575f622f34b917df079c5a4c60d1b728ea07bb2
fafa-a <fabienpuech1@gmail.com>
2022-02-21 10:32:25 +0100

    added select multiple

--- exoPanier-20220218/ex-panier-jquery.js
+++ exoPanier-20220218/ex-panier-jquery.js
@@ -1,34 +1,25 @@
-const addNewOption = (id, value) => {
-    const valueCapitalized = value.slice(0, 1).toUpperCase() + value.slice(1)
-    $(`#${id}`).append(
-        `<option value="${value.toLowerCase()}">${valueCapitalized}</option>`
-    )
-}
-const deleteOption = (id, value) =>
-    $(`#${id} option[value=${value.toLowerCase()}]`).remove()
-
 $("input[type='button']").prop("disabled", true)
+$("select").attr("multiple", true)
 
 $("#liste_fruits option").click(function () {
     $("#ajouter").prop("disabled", false)
 })
 
 $("input[id=ajouter]").click(function () {
-    const value = $("#liste_fruits option:selected").text()
-    addNewOption("panier", value)
-    deleteOption("liste_fruits", value.toLowerCase())
+    $("#panier").append($("#liste_fruits option:selected"))
+    $("#panier option:selected").prop("selected", false)
     $("#ajouter").prop("disabled", true)
 })
 
-$(document).on("change", function () {
+$(document).on("click", function () {
     $("#panier option").click(function () {
+        $("#ajouter").prop("disabled", true)
         $("#supprimer").prop("disabled", false)
     })
 })
 
 $("input[id=supprimer]").click(function () {
-    const value = $("#panier option:selected").text()
-    deleteOption("panier", value.toLowerCase())
-    addNewOption("liste_fruits", value)
+    $("#liste_fruits").append($("#panier option:selected"))
+    $("#liste_fruits option:selected").prop("selected", false)
     $("#supprimer").prop("disabled", true)
 })
